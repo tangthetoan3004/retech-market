8a155e5695b2a5ae550032787e7359a76665f3c1
tangthetoan3004 <tuitentoan3004@gmail.com>
2026-05-30 16:19:53 +0700

    Fix refactor: Correctly move all backend files into backend directory

--- ai_models/services/__init__.py
+++
@@ -1,5 +0,0 @@
-from .cv_service import DamageDetectionService
-from .price_service import AIPricingService
-from .model_loader import ModelLoader
-
-__all__ = ['DamageDetectionService', 'AIPricingService', 'ModelLoader']
--- tradein/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TradeinConfig(AppConfig):
-    name = 'tradein'
